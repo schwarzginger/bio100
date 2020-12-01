bf410a30824d94d37fdaf4933e77d5494aac2a82
schwarzginger <56312265+schwarzginger@users.noreply.github.com>
2020-12-01 16:25:03 -0500

    video-abstract upload

---
+++ video_abstract/Assets/SceneManagerScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public void GoToMainScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void GoToApproachScene()
+    {
+        SceneManager.LoadScene("ApproachScene");
+    }
+
+    public void GoToResultsScene()
+    {
+        SceneManager.LoadScene("ResultsScene");
+    }
+
+    public void GoToIntroScene()
+    {
+        SceneManager.LoadScene("IntroScene");
+    }
+
+    public void GoToGameScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void GoToWorldScene()
+    {
+        SceneManager.LoadScene("WorldScene");
+    }
+
+    public void GoToFiguresScene()
+    {
+        SceneManager.LoadScene("FiguresScene");
+    }
+
+    public void GoToFigures2Scene()
+    {
+        SceneManager.LoadScene("Figures2Scene");
+    }
+
+}
---
+++ video_abstract/Assets/TextEntryScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro; 
+
+
+public class TextEntryScript : MonoBehaviour
+{
+
+    public enum MODES { FADING, GAMEPLAY, TEXTBOX };
+    public MODES mode;
+
+    public TMP_Text textboxText;
+    public GameObject textbox;
+    public string[] textEntries;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mode = MODES.FADING;
+        StartCoroutine(introTextSequence());
+    }
+
+    IEnumerator introTextSequence()
+    {
+        mode = MODES.TEXTBOX;
+
+        textbox.SetActive(true);
+        textboxText.text = textEntries[0];
+        int count = 0;
+        while (count < textEntries.Length)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                count++;
+                if (count < textEntries.Length)
+                {
+                    textboxText.text = textEntries[count];
+                }
+            }
+            yield return null;
+        }
+        textbox.SetActive(false);
+
+        mode = MODES.GAMEPLAY;
+    }
+
+}
